e14ef413f4bae63a09895d60868ab63022096edb
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2019-01-20 20:10:10 +0500

    image and text based component for horizontal scrollview

--- screens/Explore.js
+++ screens/Explore.js
@@ -8,6 +8,7 @@ import {
     StatusBar,
     ScrollView,
     Text,
+    Image,
 } from "react-native";
 
 import Icon from 'react-native-vector-icons/Ionicons'
@@ -38,12 +39,29 @@ class Explore extends React.Component {
                         {/* end of SearchBar */}
                     </View>
                     {/* end of Header */}
+                    {/* root scrollview */}
                     <ScrollView scrollEventThrottle={16}>
+                        {/* main view container inside root scrollview */}
                         <View style={{ flex:1, backgroundColor: 'white', paddingTop: 20}}>
                             <Text style={{ fontSize:24, fontWeight:'700', paddingHorizontal: 20 }}>
                                 What can we help you find, Sarfaraz?
                             </Text>
+                            {/* first horizontal scrollable component view container */}
+                            <View style={{ height: 130, marginTop: 20}}>
+                                <ScrollView>
+                                    <View style={{ height:130, width: 130, marginLeft: 20, borderWidth: 0.5, borderColor: '#dddddd'}}>
+                                        <View style={{ flex: 2}}>  
+                                            <Image source={require('../assets/home.jpg')} style={{ flex:1, width:null, height:null, resizeMode:'cover'}} />
+                                        </View>
+                                        <View style={{ flex: 1, paddingLeft: 10, paddingTop: 10}}>
+                                            <Text>Home</Text>
+                                        </View>
+                                    </View>
+                                </ScrollView>
+                            </View>
+                            {/* end of first horizontal scrollable component view container */}
                         </View>
+                        {/* end of main view container */}
 
                     </ScrollView>
                 </View>
